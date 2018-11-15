9613c6bfb32a8f448e7c78f266fe519e204df60e
tianzuo <2719969254@qq.com>
2018-11-15 14:54:52 +0800

    搭建item部分

---
+++ leyou/ly-item/ly-item-service/src/main/java/com/leyou/LyItemApplication.java
@@ -0,0 +1,19 @@
+package com.leyou;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author VicterTian
+ * @version V1.0
+ * @Date 2018/11/15
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LyItemApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LyItemApplication.class, args);
+	}
+}
